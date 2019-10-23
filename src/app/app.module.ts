d92ff405f8cc69c97097eab03bdb3437212226bd
fernando <fernando.oliveira@cerc.inf.br>
2019-10-23 13:09:53 -0300

    configurando as rotas de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { LoginRoutingModule } from './auth/login/login-routing.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+import { LoginModule } from './auth/login/login.module';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LoginModule,
+    LoginRoutingModule,
+
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
