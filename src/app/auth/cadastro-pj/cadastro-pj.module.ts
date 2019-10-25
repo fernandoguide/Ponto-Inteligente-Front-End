6d1d5be0777a52454ea5c97d86e3e6f7cdcbbbc3
fernandoguide <fernando-guide@hotmail.com>
2019-10-25 14:21:11 -0300

    criando o formulario de cadastro de pj

--- src/app/auth/cadastro-pj/cadastro-pj.module.ts
+++ src/app/auth/cadastro-pj/cadastro-pj.module.ts
@@ -1,21 +1,47 @@
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CadastrarPjComponent } from './components/cadastrar-pj/cadastrar-pj.component';
-
+import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { CadastroPjComponent } from './components/cadastro-pj.components';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatSnackBarModule
+} from '@angular/material';
+
+import {CadastrarPjComponent} from './components/cadastrar-pj/cadastrar-pj.component';
+import {CadastroPjComponent} from './components/cadastro-pj.components';
+
+// import { CadastrarPjService } from '../';
+
+// import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    CadastrarPjComponent,
-    CadastroPjComponent
-  ],
   imports: [
     CommonModule,
     RouterModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    // SharedModule
+  ],
+  declarations: [
+    CadastrarPjComponent,
+    CadastroPjComponent
+  ],
+  providers: [
+    // CadastrarPjService
   ]
 })
 export class CadastroPjModule { }
