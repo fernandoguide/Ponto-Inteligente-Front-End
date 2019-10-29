d7c12a160fb916dee4998db3dcfbb3197a6c52c8
fernandoguide <fernando-guide@hotmail.com>
2019-10-29 15:57:43 -0300

    instalando a lib moment e criando os models no modulo shared

---
+++ src/app/shared/models/lancamento.model.ts
@@ -0,0 +1,12 @@
+
+export class Lancamento {
+
+  constructor(
+    public data: string,
+    public tipo: string,
+    public localizacao: string,
+    public funcionarioId: string,
+    public id?: string
+  ) { }
+
+}
